632931a0608df0689ee2a7ecebacbd827a792ba6
pelyp47 <dpelip03@gmail.com>
2025-03-15 22:02:55 +0100

    feat: favicon fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,9 +24,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="./favicon.ico" />
-      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
