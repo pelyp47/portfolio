519401f87af571ec98eeff50943625f17ec40e1c
pelyp47 <dpelip03@gmail.com>
2025-03-15 21:21:22 +0100

    feat: try to call deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,6 +15,9 @@ export default function Home() {
         />
         <ol>
           <li>
+            Dima app
+          </li>
+          <li>
             Get started by editing <code>src/app/page.tsx</code>.
           </li>
           <li>Save and see your changes instantly.</li>
